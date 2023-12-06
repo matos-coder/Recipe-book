25949ccb15fea76e2fd18496adf1258200c49820
matos-coder <63474484+matos-coder@users.noreply.github.com>
2023-12-06 15:27:09 +0300

    Update shoppinglist.service.ts

--- src/app/shopping-list/shoppinglist.service.ts
+++ src/app/shopping-list/shoppinglist.service.ts
@@ -31,7 +31,7 @@ export class ShoppinglistService {
   }
   updateIngredient(index: number, newIngredient: Ingredient){
     this.ingredients[index] = newIngredient;
-    //this.ingredientsChanged.next(this.ingredients.slice());
+    //this.ingredientsChanged.next(this.ingredients.slice());;
   }
   deleteIngredient(index: number){
     this.ingredients.splice(index, 1);
